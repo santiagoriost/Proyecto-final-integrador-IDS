5b52c1497b02c12c0d1b835d5ffaae537bc8c2ee
santiagoperezf <sperezf@fi.uba.ar>
2026-05-18 00:28:29 +0000

    cree endpoint para los "dashboard de "stats" y de "usuarrios". tambien cree los endpoints GET de de admins(nos da toda la info de un adm por busqueda de su id), endpoint de metodo get para usuarios a traves de adm(poder borrar por id a un usuario con la condicion de no borra a un usuario adm),un endpoint de metodo delete para borrar tu propia cuenta de adm sin poder borrar la de otro adm y por ultimo un endpoint delete de reseñas que solo el adm puede eliminar otras reseñas(proximamente las reseñas solo las podra eliminar el usuario que la realizo)

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,4 +1,196 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from db.db_conn import get_connection
 
 administradores_bp = Blueprint("administradores", __name__)
+
+@administradores_bp.route("/dashboard/stats", methods=["GET"])
+@jwt_required()
+def dashboard():
+   
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+        
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+        
+        return jsonify({
+            "mensaje": "Bienvenido al dashboard de administradores",
+            "estadisticas": {
+                "total_productos": 0,
+                "total_ventas": 0,
+                "total_clientes": 0,
+                "total_reservas": 0,
+                "total_usuarios": 0,
+                "total_locales": 0
+            }
+        }), 200
+    except Exception as e:
+        return jsonify({"error": "Error interno del servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@administradores_bp.route("/dashboard/usuarios", methods=["GET"])
+@jwt_required()
+def obtener_usuarios():
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+
+        cursor.execute("SELECT * FROM usuarios")
+        usuarios = cursor.fetchall()
+
+
+
+        return jsonify(usuarios), 200
+    except Exception as e:
+        return jsonify({"error": "Error interno del servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@administradores_bp.route("/administradores/<int:admin_id>", methods=["GET"])
+@jwt_required()
+def obtener_administrador(admin_id):
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+        
+
+        cursor.execute("SELECT * FROM administradores WHERE id = %s", (admin_id,))
+        admin = cursor.fetchone()
+
+        if not admin:
+            return jsonify({"error": "Administrador no encontrado"}), 404
+
+        return jsonify(admin), 200
+    except Exception as e:
+        return jsonify({"error": "Error interno del servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@administradores_bp.route("/usuarios/<int:usuario_id>", methods=["DELETE"])
+@jwt_required()
+def eliminar_usuario(usuario_id):
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+        
+        validacion_query_busqueda = "SELECT id, rol FROM usuarios WHERE id = %s"
+        cursor.execute(validacion_query_busqueda, (usuario_id,))
+        usuario_a_eliminar = cursor.fetchone()
+        
+        if not usuario_a_eliminar:
+            return jsonify({"error": "Usuario no encontrado"}), 404
+        
+        if usuario_a_eliminar["rol"] == "admin":
+            return jsonify({"error": "No se puede eliminar a un administrador"}), 403
+        
+        cursor.execute("DELETE FROM usuarios WHERE id = %s", (usuario_id,)) 
+        conn.commit()
+        return jsonify({"mensaje": "Usuario eliminado correctamente"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Error en el servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@administradores_bp.route("/administradores/<int:admin_id>", methods=["DELETE"])
+@jwt_required()
+def eliminar_administrador(admin_id):
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+
+        if usuario_logueado["id"] != admin_id:
+            return jsonify({"error": "No puedes eliminar a otro administrador"}), 403
+        
+        validation_query_admin = "SELECT id FROM administradores WHERE id = %s"
+        cursor.execute(validation_query_admin, (admin_id,))
+        admin_a_eliminar = cursor.fetchone()
+        if not admin_a_eliminar:
+            return jsonify({"error": "Administrador no encontrado"}), 404
+        
+        cursor.execute("DELETE FROM administradores WHERE id = %s", (admin_id,))
+        conn.commit()
+        
+        return jsonify({"mensaje": "Administrador eliminado correctamente"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Error en el servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+@administradores_bp.route("/reseñas/<int:reseña_id>", methods=["DELETE"])
+@jwt_required()
+def eliminar_reseña(reseña_id):
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        usuario_logueado = get_jwt_identity()
+        if usuario_logueado["rol"] != "admin":
+            return jsonify({"error": "Acceso denegado"}), 403
+        
+        validacion_query_reseña = "SELECT id FROM reseñas WHERE id = %s"
+        cursor.execute(validacion_query_reseña, (reseña_id,))
+        reseña_a_eliminar = cursor.fetchone()
+        if not reseña_a_eliminar:
+            return jsonify({"error": "Reseña no encontrada"}), 404
+
+        cursor.execute("DELETE FROM reseñas WHERE id = %s", (reseña_id,))  
+        conn.commit()
+
+        return jsonify({"mensaje": "Reseña eliminada correctamente"}), 200
+    
+    except Exception as e:
+        return jsonify({"error": "Error en el servidor"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
