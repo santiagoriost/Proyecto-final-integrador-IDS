66f574adbf7f98b7d138bd0b40b2e43c2df2f3d8
Caetano <crolando@fi.uba.ar>
2026-05-25 18:50:44 -0300

    Modificacion base + aplicacion login (provisional)

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from cafeteria_web.routes.inicio import inicio_bp
+from cafeteria_web.routes.login import login_bp
 app = Flask(__name__)
 
-app.register_blueprint(inicio_bp, url_prefix="/inicio")
+app.config["SECRET_KEY"] = "despues_vemos_que_pongo"
+
+app.register_blueprint(inicio_bp)
+app.register_blueprint(login_bp, url_prefix="/login")
 
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
