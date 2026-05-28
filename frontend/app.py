7f665cf555c7c162c2ac98aafa7d014f699b01b4
Caetano <crolando@fi.uba.ar>
2026-05-28 15:22:15 -0300

    Modificacion base + inicio + styles

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from cafeteria_web.routes.inicio import inicio_bp
-from cafeteria_web.routes.login import login_bp
+from cafeteria_web.routes.usuarios import usuarios_bp
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "despues_vemos_que_pongo"
 
 app.register_blueprint(inicio_bp)
-app.register_blueprint(login_bp, url_prefix="/login")
+app.register_blueprint(usuarios_bp, url_prefix="/usuario")
 
 if __name__ == "__main__":
     app.run(port=5002, debug=True)
