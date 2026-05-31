b13abbe98ea4037e5760f56dfd0b3d410375e2bd
Caetano <crolando@fi.uba.ar>
2026-05-31 17:50:49 -0300

    Bajas, modificaciones de productos (en proceso)

--- frontend/cafeteria_web/routes/dashboard.py
+++ frontend/cafeteria_web/routes/dashboard.py
@@ -19,4 +19,45 @@ def dashboard_productos():
     except Exception as e:
         flash('Datos no encontrados', 'error')
         return render_template('dashboard_productos.html')
+
+@dashboard_bp.route("/producto/<int:id_producto>/", methods=["GET", "POST"])
+def gestionar_producto(id_producto):
+    if request.method == "POST":
+        nombre = request.form.get("fproducto_nombre", "").strip()
+        precio = request.form.get("fproducto_precio", "").strip()
+        stock = request.form.get("fproducto_stock", "").strip()
+        tipo = request.form.get("fproducto_tipo", "").strip()
+        local = request.form.get("fproducto_local", "").strip()
+        datos = {
+            "nombre": nombre,
+            "precio": precio,
+            "stock": stock,
+            "tipo": tipo,
+            "local": local
+        }
+        try:
+            respuesta = requests.patch(f"{API_URL}/{id_producto}", json=datos)
+            if respuesta.status_code == 200:
+                flash("Producto modificado correctamente", "success")
+                return redirect(url_for('dashboard.dashboard_productos'))
+        except Exception as e:
+            flash(f"Error: {str(e)}", "error")
     
+    respuesta = requests.get(f"{API_URL}/{id_producto}")
+    if respuesta.status_code == 200:
+        datos = respuesta.json()
+        return render_template("dashboard_producto_BM.html", producto=datos)
+    flash("Producto no encontrado", "error")
+    return redirect(url_for("dashboard.dashboard_productos"))
+
+@dashboard_bp.route("/producto/<int:id_producto>/eliminar", methods=["POST"])
+def eliminar_producto(id_producto):
+    try:
+        response = requests.delete(f"{API_URL}/{id_producto}")
+        if response.status_code == 200:
+            flash("Producto eliminado correctamente", "success")
+            return redirect(url_for('dashboard.dashboard_productos'))
+        flash("No se pudo eliminar el producto", "error")
+    except Exception as e:
+        flash(f"error: {str(e)}", "error")
+    return redirect(url_for('dashboard.dashboard_productos'))
