7da2a00028c2d79cc3dd70091b10e1562678791d
Mvillegas122 <mateojotalo@gmail.com>
2026-06-18 23:10:21 -0300

    feat: agrega rutas de resenas desde windows

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,7 @@ from cafeteria_web.routes.usuarios import usuarios_bp
 from cafeteria_web.routes.dashboard import dashboard_bp
 from cafeteria_web.routes.ubicacion import ubicacion_bp
 from cafeteria_web.routes.carrito import carrito_bp
+from cafeteria_web.routes.resenas import resenas_bp
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "despues_vemos_que_pongo"
@@ -13,6 +14,7 @@ app.register_blueprint(usuarios_bp, url_prefix="/usuario")
 app.register_blueprint(dashboard_bp, url_prefix="/dashboard")
 app.register_blueprint(ubicacion_bp, url_prefix="/ubicacion")
 app.register_blueprint(carrito_bp, url_prefix="/carrito")
+app.register_blueprint(resenas_bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5002, debug=True)
